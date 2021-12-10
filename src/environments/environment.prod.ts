f0f6211e9b73aeace6b0fd97cc4cac97843e443c
dustingits <dustinderouen@me.com>
2021-12-10 14:26:20 -0600

    revert project install older angular fire and firebase 8

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB-wGRm4Ms5BQb42NDqJ29MEwQXuXTnO0s',
+    authDomain: 'dustin-portfolio-ba66b.firebaseapp.com',
+    projectId: 'dustin-portfolio-ba66b',
+    storageBucket: 'dustin-portfolio-ba66b.appspot.com',
+    messagingSenderId: '867022588706',
+    appId: '1:867022588706:web:03ebdce14e5e755e59f37b',
+    measurementId: '${config.measurementId}',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB-wGRm4Ms5BQb42NDqJ29MEwQXuXTnO0s',
+    authDomain: 'dustin-portfolio-ba66b.firebaseapp.com',
+    projectId: 'dustin-portfolio-ba66b',
+    storageBucket: 'dustin-portfolio-ba66b.appspot.com',
+    messagingSenderId: '867022588706',
+    appId: '1:867022588706:web:03ebdce14e5e755e59f37b',
+    measurementId: '${config.measurementId}',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
